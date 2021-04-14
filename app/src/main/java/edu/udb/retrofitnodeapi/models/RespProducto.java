08418b472434951964041a2ad6c7f0dce79959b9
myproject6128 <alexander.siguenza@gmail.com>
2021-04-14 11:40:55 -0600

    primera version

---
+++ app/src/main/java/edu/udb/retrofitnodeapi/models/RespProducto.java
@@ -0,0 +1,15 @@
+package edu.udb.retrofitnodeapi.models;
+
+
+public class RespProducto {
+    private boolean ok;
+    private Producto resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public Producto getResultado() {
+        return resultado;
+    }
+}
---
+++ app/src/main/java/edu/udb/retrofitnodeapi/models/Respuesta.java
@@ -0,0 +1,14 @@
+package edu.udb.retrofitnodeapi.models;
+
+public class Respuesta {
+    private boolean ok;
+    private int resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
